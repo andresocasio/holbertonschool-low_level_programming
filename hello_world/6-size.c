9167c087a47616e7d01e7d42254556f083d76d05
andres <ocasioandres001@gmail.com>
2023-10-13 13:20:11 -0400

    li

---
+++ hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - printf will print the text inside of the quotation
+ *
+ * Return: will wake the value of void = 0
+ */
+int main(void)
+{
+prinf $("zu -m32");
+return (0);
+}
