b72046b3167b43e96d1c782ca5c64e2bc047730c
andres <ocasioandres001@gmail.com>
2023-10-13 02:04:20 -0400

    is

---
+++ hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - printf will print the text inside of the quotation
+ *
+ * Return: will wake the value of void = 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
