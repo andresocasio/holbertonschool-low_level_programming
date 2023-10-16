c28bdbd77bcc57da4318b3afd053098d0ebad274
andres <ocasioandres001@gmail.com>
2023-10-15 20:22:13 -0400

    id

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
 /**
- * main - printf will print the text inside of the quotation
- *
- * Return: will wake the value of void = 0
+ * main - start de program
+ * variables: c,i,li,lli and f are variables
+ * printf: print a message followed by a expression
+ * Return: return 0 when finish
  */
 int main(void)
 {
-printf("-m32 -m64", % d);
+char c;
+int 1;
+long int li;
+long long int lli;
+float f;
+
+printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
+printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(li));
+printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lli));
+printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
