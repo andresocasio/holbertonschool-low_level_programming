1ccb6324b51af3e41e519147ab941be9cd416947
andres <ocasioandres001@gmail.com>
2023-10-24 10:32:26 -0400

    03

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - function that prints a string
+ *
+ * @str: string to print
+ *
+ * Return: None
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
